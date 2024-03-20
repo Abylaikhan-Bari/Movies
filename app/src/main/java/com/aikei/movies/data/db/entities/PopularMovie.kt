40ff8af116f669e9d29f496450fd2d2115ad195c
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2024-03-20 15:45:34 +0500

    Step 1

--- app/src/main/java/com/aikei/movies/data/db/entities/PopularMovie.kt
+++ app/src/main/java/com/aikei/movies/data/db/entities/PopularMovie.kt
@@ -1,4 +1,14 @@
 package com.aikei.movies.data.db.entities
 
-class PopularMovie {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "popular_movies")
+data class PopularMovie(
+    @PrimaryKey val id: Int,
+    val title: String,
+    val posterPath: String,
+    val releaseDate: String,
+    val rating: Double
+    // Add any other fields specific to a popular movie that might differ from a favorite movie
+)
