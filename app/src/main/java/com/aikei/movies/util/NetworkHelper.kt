7696fcba724618e342495bcd09732ce47aa5208e
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2024-03-22 15:55:45 +0500

    Minor update

--- app/src/main/java/com/aikei/movies/util/NetworkHelper.kt
+++ app/src/main/java/com/aikei/movies/util/NetworkHelper.kt
@@ -1,4 +1,29 @@
 package com.aikei.movies.util
 
-class NetworkHelper {
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+class NetworkHelper(private val context: Context) {
+
+    fun isNetworkConnected(): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return false
+            val actNw = connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                // for other device how are able to connect with Ethernet
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                // for check internet over Bluetooth
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+                else -> false
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            return connectivityManager.activeNetworkInfo?.isConnected ?: false
+        }
+    }
 }
